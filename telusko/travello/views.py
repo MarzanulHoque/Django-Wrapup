8198b9858cb4b7c24b71c7e63231f4f0c1b286e0
marzanulhoque <marzanulru17@gmail.com>
2020-12-03 09:31:27 +0600

    Special offer created for specific destinations

--- telusko/travello/views.py
+++ telusko/travello/views.py
@@ -8,19 +8,22 @@ def home(request):
     dest1.desc = 'City never sleeps'
     dest1.img = 'destination_1.jpg'
     dest1.price = 800
+    dest1.offer = False
+    
 
     dest2 = Destination()
     dest2.name = 'Hayderabad'
     dest2.desc = 'Pehle biriany ,fir sherwani'
     dest2.img = 'destination_2.jpg'
     dest2.price = 650
+    dest2.offer = True
 
     dest3 = Destination()
     dest3.name = 'Chennai'
     dest3.desc = 'Good city for Tech'
     dest3.img = 'destination_3.jpg'
     dest3.price = 900
-
+    dest3.offer = False
     dests = [dest1, dest2, dest3]
 
     return render(request, 'index.html', {'dests': dests})
