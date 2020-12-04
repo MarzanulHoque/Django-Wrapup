8ecb0b798a3f023eb248bb6344331bbd7538744f
marzanulhoque <marzanulru17@gmail.com>
2020-12-04 08:29:14 +0600

    Registation System Designed

---
+++ telusko/accounts/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User, auth
+
+
+# Create your views here.
+def register(request):
+    if request.method == 'POST':
+
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        username = request.POST['username']
+        email = request.POST['email']
+        password1 = request.POST['password1']
+        password2 = request.POST['password2']
+        if password1 == password2:
+            if User.objects.filter(username=username).exists():
+                print('Username Already Exists')
+            elif User.objects.filter(email=email).exists():
+                    print('Email Already Exists')
+            else:
+                user = User.objects.create_user(username=username, email=email, password=password1,
+                                                first_name=first_name,
+                                                last_name=last_name)
+                user.save()
+                print('Successfully Created')
+        else:
+            print('Password Not Matching')
+
+        return redirect('/')
+
+
+    else:
+        return render(request, 'register.html')
